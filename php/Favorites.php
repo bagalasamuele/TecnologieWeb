42b10fed35e7ecdd4c838b07e3af767cdd0fa108
Samuele <62257781+bagalasamuele@users.noreply.github.com>
2023-09-10 01:31:55 +0200

    Favorite: add page in header and refact Favorite Page

--- php/Favorites.php
+++ php/Favorites.php
@@ -20,19 +20,17 @@ include "../html/header.html";
             $test = 0;
 
             $arr = array();
-            $queryR = "SELECT vintage_items.nameItem, vintage_items.imgPath FROM vintage_items INNER JOIN favorites ON vintage_items.idItem = favorites.favorite INNER JOIN users ON favorites.idUser = users.idUser WHERE users.idUser='$_SESSION[idUser]' ORDER BY vintage_items.nameItem";
+            $queryR = "SELECT vintage_items.idItem, vintage_items.nameItem, vintage_items.imgPath FROM vintage_items INNER JOIN favorites ON vintage_items.idItem = favorites.favorite INNER JOIN users ON favorites.idUser = users.idUser WHERE users.idUser='$_SESSION[idUser]' ORDER BY vintage_items.nameItem";
             $rows = $db->query($queryR);
-
             $count = mysqli_num_rows($rows);
-
             if ($count == 0) {
             ?>
                 <div class="row">
                     <div class="col">
                         <div class="text-center">
                             <h3>Non hai ancora inserito nessun oggetto tra i preferiti.</h3>
-                            <p>Torna indietro per aggiungerne alcuni:
-                                <input type="button" onclick="document.location.href='Home.php';" value="Home" class="btn btn-primary"></p>
+                            <p>Torna indietro per aggiungerne alcuni:<br>
+                                <input type="button" onclick="document.location.href='Home.php';" value="Home" class="btn btn-primary mt-4"></p>
                         </div>
                     </div>
                 </div>
@@ -43,6 +41,7 @@ include "../html/header.html";
 
             foreach ($rows as $row) {
                 $arr[] = array(
+                    'idItem' => $row['idItem'],
                     'nomeOggetto' => $row['nameItem'],
                     'imgOggetto' => $row['imgPath']
                 );
@@ -55,8 +54,9 @@ include "../html/header.html";
                         <img src="<?= $stampa["imgOggetto"] ?>" class="card-img-top" alt="<?= $stampa["nomeOggetto"] ?>">
                         <div class="card-body">
                             <h5 class="card-title"><?= $stampa["nomeOggetto"] ?></h5>
-                            <form action="Object.php" method="get">
-                                <button type="submit" class="btn btn-primary" name="id" value="<?= $stampa["nomeOggetto"] ?>">Vai al tuo oggetto</button>
+                            <form action="../pages/Object.php" method="get">
+                                <input type="hidden" name="id" value="<?= $stampa["idItem"] ?>">
+                                <button type="submit" class="btn btn-primary">Vai al tuo oggetto</button>
                             </form>
                         </div>
                     </div>
